7acfc0e7796039734e3b0b182d8da52c919698fe
Alihasan06 <alihassanraja06@gmail.com>
2025-01-13 03:15:14 -0800

    `Update app/page.tsx: add background color, fix code formatting, and display additional student data`

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,29 @@
-
 import { client } from "@/sanity/lib/client";
 
-
 async function getData() {
-  const fetchdata = await client.fetch(`*[_type == 'student' ]`)
-  return fetchdata
+  const fetchdata = await client.fetch(`*[_type == 'student' ]`);
+  return fetchdata;
 }
 export default async function Home() {
   const data = await getData();
   console.log(data);
-  
+
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center bg-grey-900 justify-between p-24">
         <h1>Sanity Intro</h1>
-        {
-          data.map((val:any,i:number)=>{
-            return(
-              <>
-              <h1>{val.name}</h1>
-              </>
-            )
-          })
-        }
+        {data.map((val: any, i: number) => {
+          return (
+            <>
+              <div className="mt-4 ">
+                <h1>{val.name}</h1>
+                <h1>{val.age}</h1>
+                <h1>{val._type}</h1>
+                <h1>{val._id}</h1>
+              </div>
+            </>
+          );
+        })}
       </main>
     </>
   );
